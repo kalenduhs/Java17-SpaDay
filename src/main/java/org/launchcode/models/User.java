2c89e19e2410adee6156d35df9f8dab1af9f1608
kalenduhs <kalenduhs@gmail.com>
2024-01-25 20:16:30 -0600

    Completed studio signup pt2

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,18 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank
+    @Size(min = 5, max = 15, message = "Please enter a username between 5 and 15 characters")
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6, message = "Please enter a password with at least 6 characters")
     private String password;
 
     public User() {
